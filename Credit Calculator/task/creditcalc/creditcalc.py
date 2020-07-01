b595bd540ac3e7aaa77510c79149fac4bd0c301e
Grigory Rodimov <grishanyar@gmail.com>
2020-07-01 19:25:45 +0300

    Credit Calculator: stage #2

--- Credit Calculator/task/creditcalc/creditcalc.py
+++ Credit Calculator/task/creditcalc/creditcalc.py
@@ -1,11 +1,29 @@
-credit_principal = 'Credit principal: 1000'
-final_output = 'The credit has been repaid!'
-first_month = 'Month 1: paid out 250'
-second_month = 'Month 2: paid out 250'
-third_month = 'Month 3: paid out 500'
+credit_principal = int(input('Enter the credit principal: \n'))
 
-print(credit_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+while True:
+    payment_choise = input('''What do you want to calculate?
+type \"m\" - for count of months,
+type \"p\" - for monthly payment:\n''')
+    if payment_choise == 'm':
+        monthly_payment = int(input('Enter monthly payment:\n'))
+        months = credit_principal // monthly_payment
+        if months == 1:
+            print(f'It takes {months} month to repay the credit')
+            break
+        else:
+            if credit_principal % monthly_payment == 0:
+                print(f'It takes {months} months to repay the credit')
+                break
+            else:
+                print(f'It takes {months + 1} months to repay the credit')
+                break
+    elif payment_choise == 'p':
+        count_of_months = int(input('Enter count of months:\n'))
+        monthly_payment = credit_principal // count_of_months + 1
+        last_payment = credit_principal - (count_of_months - 1) * monthly_payment
+        if credit_principal % count_of_months == 0:
+            print(f'Your monthly payment = {credit_principal // count_of_months}')
+            break
+        else:
+            print(f'Your monthly payment = {monthly_payment} with last month payment = {last_payment}')
+            break
